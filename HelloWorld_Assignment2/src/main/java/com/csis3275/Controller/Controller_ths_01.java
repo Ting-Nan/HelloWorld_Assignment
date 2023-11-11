2baa09047030861caf36868b96a8bde9bec289e3
Ting-Nan <michael0170537@gmail.com>
2023-11-11 02:41:43 -0800

    revise errors

--- HelloWorld_Assignment2/src/main/java/com/csis3275/Controller/Controller_ths_01.java
+++ HelloWorld_Assignment2/src/main/java/com/csis3275/Controller/Controller_ths_01.java
@@ -10,9 +10,9 @@ public class Controller_ths_01 {
 	@GetMapping("/profile")
     public String userProfile(Model model) {
 		TingNan_HelloWorld user = new TingNan_HelloWorld();
-        user.setUsername("Your Username");
-        user.setBio("This is your personalized bio.");
-        user.setPhotoUrl("/images/your_photo.jpg");
+        user.setUsername("Ting-Nan Hsia");
+        user.setBio("Hi, my name is Ting-Nan Hsia an I am studying in Douglas College. My program is CSIS. Nice to meet you.");
+        user.setPhotoUrl("/image/picture_ths_01.jpeg");
 
         model.addAttribute("user", user);
         return "profile";
