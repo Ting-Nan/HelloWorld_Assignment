a0e4bb1222ebbe11af23c8c2ce246f10156ece01
ThanhHungDo <146279024+ThanhHungDo@users.noreply.github.com>
2023-11-13 20:40:16 -0800

    Update Controller_ths_01.java

--- HelloWorld_Assignment2/src/main/java/com/csis3275/Controller/Controller_ths_01.java
+++ HelloWorld_Assignment2/src/main/java/com/csis3275/Controller/Controller_ths_01.java
@@ -28,6 +28,19 @@ public class Controller_ths_01 {
 		model.addAttribute("user2", user2);
 		return "profile2";
 	}
+
+	@GetMapping("/homepage")
+	public String homepage(Model model) {
+	    TingNan_HelloWorld user = new TingNan_HelloWorld();
+	    ThanhHung_HelloWorld user2 = new ThanhHung_HelloWorld();
+	    user.setPhotoUrl("/image/picture_ths_01.jpeg");
+	    user2.setPhotoUrl("/image/picture_ths_02.jpg");
+	
+	    model.addAttribute("user", user);
+	    model.addAttribute("user2", user2);
+	    return "homepage";
+	}
+
 	
 	
     
